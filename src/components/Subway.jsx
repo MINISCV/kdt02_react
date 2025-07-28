cb79e045592424c78f4f5c4c1e6127526ddf5ab6
miniscv <ahdzl126@naver.com>
2025-07-28 16:50:56 +0900

    https 변경

--- src/components/Subway.jsx
+++ src/components/Subway.jsx
@@ -13,7 +13,7 @@ export default function Subway() {
   const getFetchData = async () => {
     // const KST = new Date(new Date().getTime() + 8 * 60 * 60 * 1000).toISOString().substring(0, 13).replaceAll('-', '').replace('T', '');
     const KST = new Date(new Date().getTime() + 8 * 60 * 60 * 1000).toISOString().substring(0, 10).replaceAll('-', '');
-    const url = `http://apis.data.go.kr/6260000/IndoorAirQuality/getIndoorAirQualityByStation?serviceKey=${import.meta.env.VITE_DATA_API}&pageNo=1&numOfRows=12&resultType=json&controlnumber=${KST}&areaIndex=${areaRef.current.value}`;
+    const url = `https://apis.data.go.kr/6260000/IndoorAirQuality/getIndoorAirQualityByStation?serviceKey=${import.meta.env.VITE_DATA_API}&pageNo=1&numOfRows=12&resultType=json&controlnumber=${KST}&areaIndex=${areaRef.current.value}`;
     const resp = await fetch(url);
     const data = await resp.json();
     setData(data.response.body.items.item.sort((a, b) => a.controlnumber - b.controlnumber));
